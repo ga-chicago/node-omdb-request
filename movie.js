d85a13352a839e33a74c5f22397526c1204385f5
Bridjet Mendyuk <bjmendy@Bridjets-MBP.attlocal.net>
2017-03-01 00:06:54 -0600

    homework done

---
+++ movie.js
@@ -0,0 +1,27 @@
+// The movie file will contain a Module.
+// This module should export a function named get(movieTitle).
+// get(movieTitle) should make a request to OMDB with the movieTitle as an argument.
+// You should console.log your output.
+// You should test this to verify it works!
+
+
+
+//movie poster request//
+
+function getMovie(movieTitle){
+var request = https.get("http://img.omdbapi.com/?apikey=" + key + "&" + function(response){
+   console.log('got response' + response.statusCode)
+
+   var body = "";
+})
+
+
+
+
+
+
+
+
+
+
+module.exports.get = threeFavouriteMovies;
